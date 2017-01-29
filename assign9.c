cb64cca7bc5357e00ffaf23fd360552d02f9d94b
naikpranav95 <naikpranav95@gmail.com>
2017-01-29 11:36:59 +0530

    Add files via upload

---
+++ assign9.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+
+int BinSearch(int *a,int low,int high,int key){
+
+	if(low>high)
+		return -1;
+	
+	int mid = low + (high-low)/2;//(low+high) may overflow
+	if(a[mid] == key)
+		return mid;
+	else if(key<a[mid])
+		return BinSearch(a,low,mid-1,key);
+	else
+		return BinSearch(a,mid+1,high,key);	
+	
+}
+
+
+int main(){
+	int a[10],n;
+	printf("Enter the number of elements to be added: \n");
+	scanf("%d",&n);
+
+	printf("Enter the elements: \n");
+	for(int i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+
+	printf("The entered elements are: \n");
+	for(int i=0;i<n;i++){
+		printf("%d\n",a[i]);
+	}
+	
+	int temp;
+	for(int i=0;i<n;i++){
+		for(int j=i+1;j<n;j++){
+			if(a[i]>a[j]){
+				temp = a[i];
+				a[i] = a[j];	
+				a[j] = temp;
+			}
+		}
+	}
+
+	printf("The sorted numbers are: \n");
+	for(int i=0;i<n;i++){
+		printf("%d\n",a[i]);
+	}
+
+	int key;
+	printf("Enter the number to be searched: \n");
+	scanf("%d",&key);
+
+	int low=0,high=n-1;
+	int search = BinSearch(a,low,high,key);
+
+	if(search>=0)
+		printf("The number is in the array and at position %d\n",search+1);
+	else
+		printf("The number is not in the array\n");
+	return 0;
+}
