46d91e7a77c2343bf672aaaed0b7d7c9963fbfc4
naikpranav95 <naikpranav95@gmail.com>
2017-01-30 12:05:07 +0530

    Add files via upload

---
+++ assign13.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int data;
+	struct node* prev;
+	struct node* next;
+};
+
+struct node* head;
+
+struct node* GetNewNode(int x){
+	struct node* newNode = (struct node*) malloc(sizeof(struct node));
+	newNode->data = x;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	return newNode;
+}
+
+void insertAtHead(int x){
+	struct node* newNode = GetNewNode(x);
+	if(head == NULL){
+	  head = newNode;
+	  return;
+	}
+	head->prev = newNode;
+	newNode->next = head;
+	head = newNode;
+}
+
+void deleteFirst(){
+	struct node* temp = head;
+	head = temp->next;
+	temp = NULL;
+}
+
+void display(){
+	struct node* temp = head;
+	while(temp != NULL){
+	  printf("%d\t",temp->data);
+	  temp = temp->next;
+	}
+	printf("\n");
+}
+
+int main(){
+	int n,i,x;
+	head = NULL;
+	printf("How many numbers to be inserted: \n");
+	scanf("%d",&n);
+	printf("Enter the numbers: \n");
+	for(i=0;i<n;i++){
+	  scanf("%d",&x);
+	  insertAtHead(x);
+	}
+	printf("The list is: \n");
+	display();
+	deleteFirst();
+	printf("The list after deleting the first element: \n");
+	display();
+	return 0;
+}
